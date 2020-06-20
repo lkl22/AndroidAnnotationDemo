e7e51abdce114332b525605cac1482fa426f8c9b
likunlun <sz.lkl@163.com>
2020-06-20 17:10:15 +0800

    add docs

--- app/src/main/java/com/lkl/androidannotationdemo/runtime/db/SQLInteger.java
+++ app/src/main/java/com/lkl/androidannotationdemo/runtime/db/SQLInteger.java
@@ -11,9 +11,9 @@ import java.lang.annotation.Target;
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface SQLInteger {
-    //该字段对应数据库表列名
+    // 该字段对应数据库表列名
     String name() default "";
 
-    //嵌套注解
+    // 嵌套注解，default @Constraints要求Constraints里的filed都必须有default值
     Constraints constraint() default @Constraints;
 }
